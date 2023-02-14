e2a40e2137bf7450055395e2d18fac1ab4d17d0f
Bhavani Darbha <bp.darbha@in.ibm.com>
2023-02-14 16:35:49 +0530

    add variables

--- test.tf
+++ test.tf
@@ -11,8 +11,20 @@ variable "aws_image_size" {
   default = "t2.small"
 }
 
+variable "public_ssh_key" {
+}
+variable "public_ssh_key_name" {
+}
+
+resource "aws_key_pair" "mykey" {
+  public_key = var.public_ssh_key
+  key_name = var.public_ssh_key_name
+  
+}
+
 resource "aws_instance" "my-instance1" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.aws_image_size
+  key_name = aws_key_pair.mykey.id
   tags          = { Name = "my-instance1" }
 }
