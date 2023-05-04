0c8ceff243c5ec1eb65dffb3b824bb20f3d1e596
Bhavani Pradeep Darbha <49880894+dbpradeep@users.noreply.github.com>
2023-05-04 15:11:05 +0530

    Create main.tf

---
+++ AWS/main.tf
@@ -0,0 +1,43 @@
+module "camtags" {
+  source = "../Modules/camtags"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
+}
+
+variable "instance_name" {
+  default = "awssinglevm"
+}
+    
+variable "aws_image_size" {
+  default = "t2.small"
+}
+
+variable "public_ssh_key" {
+}
+variable "public_ssh_key_name" {
+}
+
+resource "aws_key_pair" "mykey" {
+  public_key = var.public_ssh_key
+  key_name = var.public_ssh_key_name
+  
+}
+
+resource "aws_instance" "my-instance1" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.aws_image_size
+  key_name = aws_key_pair.mykey.id
+  tags = merge(
+    module.camtags.tagsmap,
+    {
+      "Name" = var.instance_name
+    },
+  )
+}
