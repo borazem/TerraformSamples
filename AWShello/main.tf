67c3e05b5c9fefc3f47569c365656e696be57bcf
Bhavani Pradeep Darbha <49880894+dbpradeep@users.noreply.github.com>
2024-05-27 14:21:54 +0530

    add keyname

--- AWShello/main.tf
+++ AWShello/main.tf
@@ -3,11 +3,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "public_ssh_key_name" {
+  description = "Name of the public SSH key used to connect to the virtual guest"
+  default     = "bptest00527052401"
+}
+
 # Create an EC2 instance
 resource "aws_instance" "bp-template270524001" {
   ami           = "ami-03e9149278a6f457c"
   instance_type = "t2.nano"
-  key_name      = "bptest00527052401"
+  key_name      = var.public_ssh_key_name
 
   tags = {
     Name = "bp-template270524001"
