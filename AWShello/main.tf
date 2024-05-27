d7db3fa8729f45f10cf632d77aa581a13df0e9de
Bhavani Pradeep Darbha <49880894+dbpradeep@users.noreply.github.com>
2024-05-27 12:56:47 +0530

    Create main.tf

---
+++ AWShello/main.tf
@@ -0,0 +1,15 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create an EC2 instance
+resource "aws_instance" "bp-template270524001" {
+  ami           = "ami-785db401"
+  instance_type = "t2.nano"
+  key_name      = bp-template270524001
+
+  tags = {
+    Name = "bp-template270524001"
+  }
+}
