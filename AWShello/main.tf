656f3ed13dd5517db83b23b99f7eabcf903f275b
Bhavani Pradeep Darbha <49880894+dbpradeep@users.noreply.github.com>
2024-06-03 00:33:23 +0530

    make instance name as parameter

--- AWShello/main.tf
+++ AWShello/main.tf
@@ -3,18 +3,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "stack_name" {
+  type    = string
+  default = "awssamplevm"
+}
 variable "public_ssh_key_name" {
   description = "Name of the public SSH key used to connect to the virtual guest"
   default     = "bptest00527052401"
 }
 
 # Create an EC2 instance
-resource "aws_instance" "bp-template010624001" {
+resource "aws_instance" "bp_sample_template" {
   ami           = "ami-03e9149278a6f457c"
   instance_type = "t2.nano"
   key_name      = var.public_ssh_key_name
 
   tags = {
-    Name = "bp-template010624001"
+    Name = var.stack_name
   }
 }
